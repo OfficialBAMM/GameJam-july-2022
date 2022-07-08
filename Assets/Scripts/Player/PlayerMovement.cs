8b54fe10488e7103ee517be6cca1543b474b4605
Robbert Ritsema <robbert@notfound.nl>
2022-07-08 21:06:36 +0200

    Added player prefab

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float movementSpeed = 7.5f;
+
+    private Vector2 input;
+
+    private void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    public void FixedUpdate()
+    {
+        rb.velocity = input * movementSpeed;
+    }
+}
---
+++ Assets/Scripts/Player/State.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected Player player;
+
+    public abstract void Tick();
+    public abstract void FixedTick();
+
+    public virtual void OnStateEnter() { }
+    public virtual void OnStateExit() { }
+
+    public State(Player player)
+    {
+        this.player = player;
+    }
+}
