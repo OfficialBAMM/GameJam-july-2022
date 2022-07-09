ef1bff3dffc14572d523e96209c3e5746a6acd40
Drummyralf <drummyralf@gmail.com>
2022-07-09 15:41:46 +0200

    Post Processing

--- Assets/PostProcessing.cs
+++ Assets/PostProcessing.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class PostProcessing : MonoBehaviour
 {
-    private PostProcessing postProcessing;
+    PostProcessVolume volume;
+    private Vignette vignette;
+    [SerializeField] float vignetteStartingValue = 0.1f;
+    [SerializeField] float vignetteEndingValue = 1;
+    [SerializeField] float vignetteSpeed = 0.51f;   
 
     private void OnEnable()
     {
-        EventManager.gameInterrupted += ActivateVignet();
+        EventManager.gameInterrupted += ActivateVignet;
+        volume = GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out vignette);
+        vignette.intensity.value = 0;
+        Invoke("StartEvent", 30);
     }
 
     private void OnDisable()
     {
-        EventManager.gameInterrupted -= ActivateVignet();
+        EventManager.gameInterrupted -= ActivateVignet;
+    }
+
+    private void Update()
+    {
+        
     }
 
     public void ActivateVignet()
     {
-        
+        vignette.intensity.value = vignetteStartingValu
+    }
+
+    public void StartEvent()
+    {
+        EventManager.GameIsInterrupted();
     }
 }
