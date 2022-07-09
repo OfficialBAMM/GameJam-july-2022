68020ca357d7e2a4e92e68f166286f912408cb2b
Drummyralf <drummyralf@gmail.com>
2022-07-09 23:00:02 +0200

    Push Light

---
+++ Assets/LightBulb.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBulb : MonoBehaviour
+{
+    [SerializeField] Image imageLightOn;
+    [SerializeField] Image imageLightOff;
+
+    void TurnOnLight()
+    {
+
+    }
+
+    void TurnOffLight()
+    {
+
+    }
+}
