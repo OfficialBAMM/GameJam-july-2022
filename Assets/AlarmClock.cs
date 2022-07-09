22ab2f8618594f47126eb0ea85d6dc6e2c89b43a
Drummyralf <drummyralf@gmail.com>
2022-07-09 11:05:24 +0200

    Pillow Ammo

--- Assets/AlarmClock.cs
+++ Assets/AlarmClock.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AlarmClock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    [SerializeField] Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayAnimation()
+    {
+        animator.SetTrigger("Pressed");
     }
 }
